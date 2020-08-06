a87079f88d7662a32a7ab6431f5e111d65eb3afb
SquiddyPoos <34890855+SquiddyPoos@users.noreply.github.com>
2020-08-06 22:41:47 +0800

    Update internal.js

--- app/static/js/internal.js
+++ app/static/js/internal.js
@@ -2,6 +2,7 @@ function fetch_coords() {
 	fetch('./fetch-data')
 	.then(response => response.json())
 	.then(function(data) {
-		document.getElementById("mapframe").src.replace(/&q=.+/, "&q="+data.lat+"+"+data.long);
+		var new_src = document.getElementById("mapframe").src.replace(/&q=.+/, "&q="+data.lat+"+"+data.long);
+		document.getElementById("mapframe").src = new_src;
 	})
 }
