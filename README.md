8310eb162017457aaa9850a1a85a69a0e4fe7f7c
SquiddyPoos <squid2015guy@gmail.com>
2020-10-07 16:19:44 +0800

    readme/setup changes

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# cartracker
+# GPS Car Tracker
+The code for the box is in /waveshare \
+The code for the part in the car is in /cartracker \
+Miscellaneous code for setting up LoRas can be found in /setup_lora \
+Waveshare_ILI9486_Modified.zip must be added as a zipped library. \
+Additional modules required: \
+AltSoftSerial, NeoSWSerial, toneAC
+
+### Additional notes
+If you want the raw github repositiory, run: \
+```
+git clone https://github.com/SquiddyPoos/cartracker
+``` \
+Will NOT work on Windows, unless you install git.
--- setup_lora/setup_lora.ino
+++ setup_lora/setup_lora.ino
@@ -1,8 +1,19 @@
 #include <SoftwareSerial.h>
 
-#define LO_RX_PIN 3
-#define LO_TX_PIN 2
-#define LO_SET 7
+// Configuration: change settings here
+#define LO_RX_PIN 3     /* Note: the one labeled "TX", not "RX" */
+#define LO_TX_PIN 2     /* Note: the one labeled "RX", not "TX" */
+#define LO_SET    7     /* Labeled "SET" */   
+#define FREQUENCY 28
+/* Values below are in hexadecimal. You can use 00 - FF. */
+/* Do not remove the 0x part. Only use the last 2 digits. */
+#define NET_ID_1  0x12 
+#define NET_ID_2  0x12
+#define NET_ID_3  0x10
+#define NET_ID_4  0x12
+#define NODE_ID_1 0x00
+#define NODE_ID_2 0x0A
+// Do not modify below code.
 
 SoftwareSerial lora(LO_RX_PIN, LO_TX_PIN);
 
@@ -14,7 +25,7 @@ void setup() {
   digitalWrite(A2, LOW);
   lora.begin(9600);
   delay(100);
-  byte command[33] = {0xAA, 0xFA, 0x03, 28, 0x04, 0x03, 0x07, 0x03, 0x02, 0x01, 0x01, 0x12, 0x12, 0x10, 0x12, 0x00, 10, 0x00, 0x00, 0x00, 0x00, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+  byte command[33] = {0xAA, 0xFA, 0x03, FREQUENCY, 0x04, 0x03, 0x07, 0x03, 0x02, 0x01, 0x01, NET_ID_1, NET_ID_2, NET_ID_3, NET_ID_4, NODE_ID_1, NODE_ID_2, 0x00, 0x00, 0x00, 0x00, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
   lora.write(command, 33);
   Serial.begin(9600);
 }
